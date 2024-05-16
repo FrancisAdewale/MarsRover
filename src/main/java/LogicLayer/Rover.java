6a73a7661e5b97f9030184c34c646959aa77d2e0
Francis Adewale <francisadewale1992@gmail.com>
2024-05-16 16:18:34 +0100

    update: implementing Logic layer

--- src/main/java/LogicLayer/Rover.java
+++ src/main/java/LogicLayer/Rover.java
@@ -1,5 +1,6 @@
 package LogicLayer;
 
+import InputLayer.Direction;
 import InputLayer.Instruction;
 import InputLayer.PlateauSize;
 import InputLayer.Position;
@@ -31,6 +32,35 @@ public class Rover {
         System.out.println("Starting Position: " + this.startingPosition.toString());
 
         for(Instruction i : instructionList) {
+            if(startingPosition.getFacing().name().equalsIgnoreCase("n")) {
+                if(i.name().equalsIgnoreCase("l")){
+                    startingPosition.setFacing(Direction.W);
+                } else if(i.name().equalsIgnoreCase("r")) {
+                    startingPosition.setFacing(Direction.E);
+                } else {
+                    plateau.getPlateauSize()[startingPosition.getX()][startingPosition.getY() + 1] = "/";
+                }
+            } else if(startingPosition.getFacing().name().equalsIgnoreCase("e")) {
+                if(i.name().equalsIgnoreCase("l")){
+                    startingPosition.setFacing(Direction.N);
+                } else if(i.name().equalsIgnoreCase("r")) {
+                    startingPosition.setFacing(Direction.S);
+                } else {
+                    plateau.getPlateauSize()[startingPosition.getX() + 1][startingPosition.getY()] = "/";
+                }
+            } else if(startingPosition.getFacing().name().equalsIgnoreCase("s")) {
+                if(i.name().equalsIgnoreCase("l")) {
+                    startingPosition.setFacing(Direction.E);
+                } else if (i.name().equalsIgnoreCase("r")) {
+                    startingPosition.setFacing(Direction.W);
+                } else {
+                    plateau.getPlateauSize()[startingPosition.getX()][startingPosition.getY() + 1] = "/";
+                }
+            } else {
+                if(i.name().equalsIgnoreCase("l")) {
+                    startingPosition.setFacing(Direction.S);
+                }
+            }
 
         }
 
