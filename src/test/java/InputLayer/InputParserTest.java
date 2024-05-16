f196e80c14b933547846c7d023a8e785465dbccd
Francis Adewale <francisadewale1992@gmail.com>
2024-05-16 15:02:33 +0100

    update: create empty tests for input parser

---
+++ src/test/java/InputLayer/InputParserTest.java
@@ -0,0 +1,21 @@
+package InputLayer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InputParserTest {
+
+
+    @Test
+    void parsePosition() {
+    }
+
+    @Test
+    void parsePlateauSize() {
+    }
+
+    @Test
+    void parseInstruction() {
+    }
+}
