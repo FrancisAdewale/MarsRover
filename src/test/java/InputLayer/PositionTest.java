b852ea3f1b8be97d7b5b27661dd1a2facdaa6786
Francis Adewale <francisadewale1992@gmail.com>
2024-05-16 13:31:38 +0100

    update: test cases pass for Postion Direction enum

--- src/test/java/InputLayer/PositionTest.java
+++ src/test/java/InputLayer/PositionTest.java
@@ -6,17 +6,36 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class PositionTest {
 
+
+
     @Test
     void testPositionCordinates() {
         Position pos = new Position(2,4,Direction.N);
 
+
         assertAll(
                 () -> assertEquals(2, pos.getX()),
-                () -> assertEquals(4, pos.getY())
+                () -> assertEquals(4, pos.getY()),
+                () -> assertFalse(pos.getX() == 5),
+                () -> assertNotEquals(2, pos.getY())
         );
+
+    }
+
+    @Test
+    void testPositionDirection() {
+        Position pos = new Position(2,4,Direction.N);
+        assertEquals("N", pos.getFacing().name());
+
+        pos.setFacing(Direction.S);
+        assertEquals("S", pos.getFacing().name());
+        assertNotEquals("N",pos.getFacing().name());
 
 
 
 
     }
+
+
+
 }
