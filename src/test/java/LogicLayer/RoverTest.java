ba34e1581d8564448f0b3c82ff245fb7ffbabb6e
Francis Adewale <francisadewale1992@gmail.com>
2024-05-17 10:29:20 +0100

    update: test cass passes for rover moving forward once

--- src/test/java/LogicLayer/RoverTest.java
+++ src/test/java/LogicLayer/RoverTest.java
@@ -36,6 +36,21 @@ class RoverTest {
         rover.traverse();
         assertEquals(Direction.W, startingPos.getFacing());
 
+    }
+
+    @Test
+    void roverMovesForwardOnce() {
+        Plateau plateau = InputParser.parsePlateauSize("5 5");
+
+        Direction facing = Direction.N;
+        Position startingPos = new Position(2,2, facing);
+        List<Instruction> instructions = List.of(Instruction.M);
+
+        Rover rover = new Rover(startingPos,instructions,plateau);
+        rover.traverse();
+
+        assertEquals(3, startingPos.getY());
+
 
     }
 }
